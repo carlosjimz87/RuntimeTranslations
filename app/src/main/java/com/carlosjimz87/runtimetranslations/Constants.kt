b1501891504e67b3505ebe8fe0f89d6e1bdfcaf4
Carlos Jiménez <carlos.jimenezp@vass.es>
2023-08-04 10:52:21 +0200

    constants and list of languages

---
+++ app/src/main/java/com/carlosjimz87/runtimetranslations/Constants.kt
@@ -0,0 +1,7 @@
+package com.carlosjimz87.runtimetranslations
+
+object Constants {
+    val LANGUAGES = listOf("en", "es", "de")
+
+
+}
--- app/src/main/java/com/carlosjimz87/runtimetranslations/views/MainActivity.kt
+++ app/src/main/java/com/carlosjimz87/runtimetranslations/views/MainActivity.kt
@@ -3,6 +3,7 @@ package com.carlosjimz87.runtimetranslations.views
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
+import com.carlosjimz87.runtimetranslations.Constants.LANGUAGES
 import com.carlosjimz87.runtimetranslations.R
 import com.carlosjimz87.runtimetranslations.base.BaseActivity
 import com.carlosjimz87.runtimetranslations.databinding.ActivityMainBinding
@@ -34,20 +35,12 @@ class MainActivity : BaseActivity(){
         binding.button.setOnClickListener {
             startActivity(Intent(this, TestActivity1::class.java))
         }
-
-        binding.spanishBT.setOnClickListener {
-            localeManager.setLocale(Locale("es"))
-            recreate()
-        }
-
-        binding.englishBT.setOnClickListener {
-            localeManager.setLocale(Locale("en"))
-            recreate()
-        }
-
-        binding.germanBT.setOnClickListener {
-            localeManager.setLocale(Locale("de"))
-            recreate()
+        val localesButtons = mutableListOf(binding.englishBT, binding.spanishBT, binding.germanBT)
+        localesButtons.zip(LANGUAGES).forEach { (button, lang) ->
+            button.setOnClickListener {
+                localeManager.setLocale(Locale(lang))
+                recreate()
+            }
         }
     }
 
